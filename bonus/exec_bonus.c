4993520f9cc80d9d6eb7b640254dbb05688469e2
Auguste Deroubaix <aderouba@2G2.42angouleme.fr>
2022-11-18 17:53:05 +0100

    plus de close de -1

--- bonus/exec_bonus.c
+++ bonus/exec_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:32:25 by aderouba          #+#    #+#             */
-/*   Updated: 2022/11/18 17:48:23 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/11/18 17:52:28 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,8 @@ void	first_exec_command(char **envp, char ***args, int i, int **fds)
 		else
 			execve(args[i][0], args[i], envp);
 	}
-	close(fds[0][0]);
+	if (fds[0][0] != -1)
+		close(fds[0][0]);
 	close(fds[1][1]);
 	waitpid(cpid, NULL, 0);
 }
@@ -97,6 +98,7 @@ void	last_exec_command(char **envp, char ***args, int i, int **fds)
 			execve(args[i][0], args[i], envp);
 	}
 	close(fds[out_i][0]);
-	close(fds[0][1]);
+	if (fds[0][1] != -1)
+		close(fds[0][1]);
 	waitpid(cpid, NULL, 0);
 }
